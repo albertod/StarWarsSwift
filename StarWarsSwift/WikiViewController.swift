6c3a629d1595cbf6134090ef03174d702fa4e3c6
Alberto Di Martino <albertodmartino@gmail.com>
2015-05-17 13:03:02 -0500

    Finished StarWarsTableViewController and started to implement WikiViewController

---
+++ StarWarsSwift/WikiViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WikiViewController.swift
+//  StarWarsSwift
+//
+//  Created by Alberto Di Martino on 5/17/15.
+//  Copyright (c) 2015 DiMartino. All rights reserved.
+//
+
+import Cocoa
+
+class WikiViewController: UIViewController {
+
+}
